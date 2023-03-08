c5afcae9daf43b3f628dbe58d51742448a39f180
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2023-03-08 20:01:42 +0100

    Configure Jest with coverage

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// jest.config.js
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+};
