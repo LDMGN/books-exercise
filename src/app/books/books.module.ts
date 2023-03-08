3e921bebcd8c3563d7645a485abf9570cc1d327f
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2023-03-08 20:07:33 +0100

    Add navigation for book-list and add-book

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { AddBookStepAComponent } from './add/add-book-step-a/add-book-step-a.component';
 import { AddBookStepBComponent } from './add/add-book-step-b/add-book-step-b.component';
 import { BookIndexComponent } from './book-index/book-index.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { AddBookComponent } from './add/add-book/add-book.component';
 
 @NgModule({
   declarations: [
     BookIndexComponent,
     BookListComponent,
     AddBookStepAComponent,
-    AddBookStepBComponent
+    AddBookStepBComponent,
+    AddBookComponent
   ],
   imports: [
     CommonModule,
@@ -21,8 +26,8 @@ import { BookListComponent } from './book-list/book-list.component';
         component: BookIndexComponent,
         children: [
           {
-            path: 'create',
-            component: BookListComponent,
+            path: 'add',
+            component: AddBookComponent,
           },
           {
             path: '',
@@ -31,6 +36,9 @@ import { BookListComponent } from './book-list/book-list.component';
         ]
       },
     ]),
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
   ]
 })
 export class BooksModule {
