b7150e207e62a5951916288857f7d6f785e9b164
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2023-03-08 21:57:03 +0100

    Read books from JSON

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BooksService } from '../books.service';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  public books$?: Observable<Book[]>;
+
+  constructor(private readonly _booksService: BooksService) {
+  }
+
+  public ngOnInit(): void {
+    this.books$ = this._booksService.getBooks();
+  }
 }
--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable, of, throwError } from 'rxjs';
+import JsonBookSource from './books.json';
+import { Book, isBook } from './models/book';
+
+interface JsonSource {
+  books: Book[];
+}
+
+const isJsonSource = (input: unknown): input is JsonSource =>
+  typeof input === 'object'
+  && !Array.isArray(input)
+  && Array.isArray((input as any).books)
+  && (input as JsonSource).books.every(book => isBook(book));
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
-
-  constructor() { }
+  public getBooks(): Observable<Book[]> {
+    const source = JsonBookSource;
+    console.log(source);
+    if (!isJsonSource(source)) {
+      return throwError(() => 'Error retrieving books')
+        .pipe(
+          delay(2000)
+        );
+    }
+    return of(source.books)
+      .pipe(
+        delay(2000)
+      );
+  }
 }
